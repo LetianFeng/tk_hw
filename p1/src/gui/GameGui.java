4b31e08fcc2425c966effe8f0ccb0a56644ce10b
Kecen LI <kecen@s3101.dyn.hrz.tu-darmstadt.de>
2016-11-24 00:06:00 +0100

    resolve OpenJDK 1.7 compilation issue

--- p1/src/gui/GameGui.java
+++ p1/src/gui/GameGui.java
@@ -169,7 +169,7 @@ public class GameGui implements GameGuiInterface, Runnable{
 		return this.gameFrame;
 	}
 	
-	private JLabel createLabel(UUID minionId, int x, int y) {
+	private JLabel createLabel(final UUID minionId, int x, int y) {
 		JLabel minionLabel = new JLabel(minionId.toString());
 		minionLabel.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
