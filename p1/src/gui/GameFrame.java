3e49630623e8b227baac3267c32ad3969b750709
“liushule” <chinaliushule@gmail.com>
2016-11-23 22:54:15 +0100

    optimize GUI

--- p1/src/gui/GameFrame.java
+++ p1/src/gui/GameFrame.java
@@ -1,7 +1,11 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -30,6 +34,14 @@ public class GameFrame extends JFrame{
 		protected void initialize() {
 			this.setBounds(Constant.gameFrameX, Constant.gameFrameY, Constant.gameFrameLength, Constant.gameFrameWidth);
 			this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+			System.out.print(System.getProperty("user.dir"));
+		Image image = toolkit.getImage("p1/src/gui/bananas-icon.png");
+		Point cursorHotSpot = new Point(16, 16);
+		Cursor c = toolkit.createCustomCursor(image ,cursorHotSpot, "img");
+
+		this.setCursor (c);
 			addWindowListener(new WindowAdapter() {
 				public void windowClosing(WindowEvent e) {
 					client.logout();
