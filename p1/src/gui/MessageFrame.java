4619ec9063e86dcaee77e0239f404e2acb894a02
Zhen Chen <chenzhen.seu@gmail.com>
2016-11-24 00:27:00 +0100

    close notification window

--- p1/src/gui/MessageFrame.java
+++ p1/src/gui/MessageFrame.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,13 +18,19 @@ public class MessageFrame extends JFrame{
 	
 	public void initialize(String message) {
 		this.setName(message);
-		this.setBounds(Constant.messageFrameX, Constant.messageFrameY, Constant.messageFrameWidth, Constant.messageFrameHeight);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(Constant.messageFrameX, Constant.messageFrameY, Constant.messageFrameLength, Constant.messageFrameWidth);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				disposeFrame();
+			}
+		}
+		);
 		this.getContentPane().setLayout(null);
 		
 		System.out.printf("mesage label is: "+message +"\n");
 		JLabel notifyLabel = new JLabel(message);
-		notifyLabel.setBounds(Constant.messageLabelX, Constant.messageLabelY, Constant.messageLabeWidth, Constant.messageLabelHeight);
+		notifyLabel.setBounds(Constant.messageLabelX, Constant.messageLabelY, Constant.messageLabeLength, Constant.messageLabelWidth);
 		
 		JButton btn = new JButton("OK");
 		btn.setForeground(Color.BLACK);
