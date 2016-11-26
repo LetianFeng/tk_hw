8f26620c1d2a1e5cca0ebeed9194c8091284be74
Xin Zhang <zhangxin_2002_01@163.com>
2016-11-26 16:33:18 +0100

    implement soap server

---
+++ p2/src/server/server/ServerSoapPublisher.java
@@ -0,0 +1,11 @@
+package server.server;
+
+import javax.xml.ws.Endpoint;
+
+//Endpoint publisher
+public class ServerSoapPublisher {
+
+    public static void main(String[] args) {
+        Endpoint.publish("http://localhost:9999/booking", new ServerSoap());
+    }
+}
