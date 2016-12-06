b73c109c4caece91c6f34997b6dde8be4c906559
Letian Feng <letian.feng@hotmail.com>
2016-12-06 22:36:11 +0100

    fix bugs in soap client

--- p2/src/client/ClientSoap.java
+++ p2/src/client/ClientSoap.java
@@ -52,6 +52,9 @@ public class ClientSoap implements ClientGUIInterface{
         this.startDate = startDate;
         this.endDate = endDate;
 
+        dateSetNull(startDate);
+        dateSetNull(endDate);
+
         // display start & end date from gui
         System.out.println("Search rooms in the following date range: ");
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -73,6 +76,12 @@ public class ClientSoap implements ClientGUIInterface{
 
     }
 
+    private void dateSetNull(Date date) {
+        date.setHours(1);
+        date.setMinutes(0);
+        date.setSeconds(0);
+    }
+
     @Override
     public void getExtraServices() {
         System.out.println();
@@ -83,6 +92,9 @@ public class ClientSoap implements ClientGUIInterface{
 
     @Override
     public void sendBooking(Map<String, Integer>serviceMap, String email) {
+
+        System.out.println();
+        System.out.println("Gui request send booking:");
         ArrayList<BookingReq> bookingReqList = new ArrayList<>();
 
         for (hotel.Service service : serviceList) {
@@ -90,10 +102,16 @@ public class ClientSoap implements ClientGUIInterface{
             for (String serviceName : serviceMap.keySet()) {
 
                 if (service.getType().equals(serviceName) && service.getAmount()>= serviceMap.get(serviceName)) {
+                    System.out.println("Start date: " + startDate);
+                    System.out.println("End date: " + endDate);
+
                     Date bookDate = (Date) startDate.clone();
-                    while (bookDate.before(endDate)) {
+                    while (dateBefore(bookDate, endDate)) {
+                        System.out.println("Current date: " + bookDate);
+                        System.out.println("End date: " + endDate);
                         for (int i = 0; i < serviceMap.get(serviceName); i++) {
-                            BookingReq bookingReq = new BookingReq(service.getId(), email, bookDate);
+                            BookingReq bookingReq = new BookingReq(service.getId(), email, (Date) bookDate.clone());
+                            System.out.println("add booking: " + bookingReq);
                             bookingReqList.add(bookingReq);
                         }
                         bookDate.setDate(bookDate.getDate()+1);
@@ -102,6 +120,12 @@ public class ClientSoap implements ClientGUIInterface{
             }
         }
         Gson gson =  new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        String bookingReqJSON = gson.toJson(bookingReqList);
+
+
+        System.out.println(bookingReqJSON);
+
+
         String bookingResponse = soapServer.postBookingEntry(gson.toJson(bookingReqList));
         BookingResponse response = gson.fromJson(bookingResponse, BookingResponse.class);
         if (!response.isBookingState())
@@ -109,6 +133,18 @@ public class ClientSoap implements ClientGUIInterface{
         else
             gui.drawSuccessDetails(bookingResponse);
 
+    }
+
+    private boolean dateBefore(Date startDate, Date endDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String start = dateFormat.format(startDate);
+        String end = dateFormat.format(endDate);
+        start = start.replaceAll("-", "");
+        end = end.replaceAll("-", "");
+        int startInt = Integer.parseInt(start);
+        int endInt = Integer.parseInt(end);
+        System.out.println(startInt + " " + endInt);
+        return startInt < endInt;
     }
 
     @Override
